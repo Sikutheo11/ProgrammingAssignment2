6624dd9ba5b4c47cbaa0e1783bd39da5dce64a9f
Sikutheo11 <sikutheo11@gmail.com>
2020-08-26 09:28:22 -0800

    getting resdy to push an axample cachematrix file

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,25 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## we created two function for computing inverse of matrix (makeCacheMatrix, cacheSolve)
+## below makecahematrix consist (set, get, getInverse , setInverse)
+makeCacheMatrix <- function(x = matrix()){
+  inv <- NULL  ##initializing inverse to be NULL
+  set <- function(y){
+    x <<- y
+    inv <<- NULL 
+  }
+  get <- function() {x} ## function for getting matrix
+  setInverse <- function(inverse)  {inv <<- inverse}
+  getInverse <- function() {inv} ## function for computing inverse of matrix
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## below function id=s for getting cache data
+cacheSolve <-  function(x, ...){ ## getting cache data
+  inv <- x$getInverse()
+  if(!is.null(inv)){ ## check if inverse is NULL
+    message("getting cached data")
+    return(inv) ## values of matrix inverse
+  }
+  mat <-x$get() ## computing inverse values
+  inv <- solve(mat, ...)
+  x$setInverse(inv) 
+  inv
 }
